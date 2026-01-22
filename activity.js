72b0002af566c9652fe11687725855bc7b8ab06a
Kenzie-Crawford <kenzie.scrawford13@gmail.com>
2026-01-22 14:15:14 -0500

    trip questions and comparisons

--- activity.js
+++ activity.js
@@ -60,7 +60,7 @@ const isFirstLarger= n1 > n4;
 console.log(isFirstLarger);
 
 //Subtract the first number from the second number.
-const subtractFirstFromLast= n2- n4;
+const subtractFirstFromLast= n2 - n4;
 console.log(subtractFirstFromLast);
 //Multiply the result by the third number.
 const multiplyByThird = subtractFirstFromLast * n3;
@@ -75,3 +75,71 @@ console.log(remainderByFourth);
 // in other logic comparisons. Rename the variable as appropriate.
 const isUnder25 = n1 <= 25 && n2 <= 25 && n3 <= 25 && n4 <= 25;
 console.log(isUnder25);
+
+
+
+
+//The distance of the trip, in total, is 1,500 miles.
+
+//Your car’s fuel efficiency is as follows:
+
+//At 55 miles per hour, you get 30 miles per gallon.
+
+//At 60 miles per hour, you get 28 miles per gallon.
+
+//At 75 miles per hour, you get 23 miles per gallon.
+//You have a fuel budget of $175.
+
+//The average cost of fuel is $3 per gallon.
+const totalDistance = 1500;
+const gasCostPerGallon = 3;
+const gasBudget = 175; 
+const mpgAt55 = 30;
+const mpgAt60 = 28;
+const mpgAt75 = 23;
+//Set up a program to answer the following questions:   
+//How many gallons of fuel will you need for the entire trip?
+
+let gallonsNeededAt55 = totalDistance / mpgAt55;
+console.log(gallonsNeededAt55);
+
+let gallonsNeededAt60 = totalDistance / mpgAt60;
+console.log(gallonsNeededAt60);
+
+let gallonsNeededAt75 = totalDistance / mpgAt75;
+console.log(gallonsNeededAt75);
+
+//Will your budget be enough to cover the fuel expense?
+let gasExpenseAt55 = gallonsNeededAt55 * gasCostPerGallon;
+console.log(gasExpenseAt55);
+let isBudgetSufficientAt55 = gasExpenseAt55 <= gasBudget;
+console.log(isBudgetSufficientAt55);
+
+let gasExpenseAt60 = gallonsNeededAt60 * gasCostPerGallon;
+console.log(gasExpenseAt60);
+let isBudgetSufficientAt60 = gasExpenseAt60 <= gasBudget;
+console.log(isBudgetSufficientAt60);
+
+let gasExpenseAt75 = gallonsNeededAt75 * gasCostPerGallon;
+console.log(gasExpenseAt75);
+let isBudgetSufficientAt75 = gasExpenseAt75 <= gasBudget;
+console.log(isBudgetSufficientAt75);  
+
+//How long will the trip take, in hours?
+
+let tripTimeAt55 = totalDistance / 55;
+console.log(tripTimeAt55);
+
+let tripTimeAt60 = totalDistance / 60;
+console.log(tripTimeAt60);
+
+let tripTimeAt75 = totalDistance / 75;
+console.log(tripTimeAt75);
+//Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+
+// Traveling at 75 miles per hour will take the least amount of time, but it is the most expensive. 
+// Traveling at 55 miles per hour is the most fuel-efficient and cheapest option, but it takes the most time.
+// Traveling at 60 miles per hour seems to be the best middle-ground between cost and time. 
+
+
+
